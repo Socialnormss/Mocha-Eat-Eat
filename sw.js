621dde14e200c42bd6cddf15ba849ef2d14e5d11
Social Norms <liewsk129@gmail.com>
2026-05-18 15:25:35 +0700

    Fix codes JN/LL, Kcal auto-calc, Mocha icon, native-app viewport

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'dog-feeder-v1';
-const ASSETS = ['./index.html', './nutrition.html', './manifest.json', './icon.svg'];
+const ASSETS = ['./index.html', './nutrition.html', './manifest.json', './icon.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
